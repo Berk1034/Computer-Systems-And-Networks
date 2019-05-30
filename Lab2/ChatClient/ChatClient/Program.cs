b642d5a265da7a2f5c416f58814ec4df3314b562
Berk1034 <44654376+Berk1034@users.noreply.github.com>
2019-05-30 20:28:51 +0300

    Lab2 Completed

---
+++ Lab2/ChatClient/ChatClient/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static string userName;
+        static string host;
+        static int port;
+        static TcpClient client;
+        static NetworkStream stream;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter ip: ");
+            host = Console.ReadLine();
+            Console.WriteLine("Enter port: ");
+            try
+            {
+                port = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Wrong port.");
+                System.Threading.Thread.Sleep(5000);
+                Environment.Exit(0);
+            }
+            Console.Write("Enter your nickname: ");
+            userName = Console.ReadLine();
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port);
+                stream = client.GetStream();
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start();
+                Console.WriteLine("Welcome, {0}!", userName);
+                SendMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                System.Threading.Thread.Sleep(5000);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        static void SendMessage()
+        {
+            Console.WriteLine("Enter the message: ");
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+
+        static void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message);
+                }
+                catch
+                {
+                    Console.WriteLine("Connection lost!");
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();
+            if (client != null)
+                client.Close();
+            Environment.Exit(0);
+        }
+    }
+}
